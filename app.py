d524d9ad0e4f8ed94f4357540e6bcbe4d4e77328
Ramon-Bravo <113270267+Ramon-Bravo@users.noreply.github.com>
2024-10-16 20:12:43 -0600

    intento despliegue en render

--- app.py
+++ app.py
@@ -2,15 +2,17 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-car_data = pd.read_csv('C:/Users/packe/OneDrive/Documentos/proyecto_sprint_6/vehicles_us.csv') # leer los datos
+car_data = pd.read_csv(
+    'C:/Users/packe/OneDrive/Documentos/proyecto_sprint_6/vehicles_us.csv')  # leer los datos
 
 st.header("Analisis de datos Proyecto 6")
 
-hist_button = st.button('Construir histograma') # crear un botón
+hist_button = st.button('Construir histograma')  # crear un botón
 
-if hist_button: # al hacer clic en el botón
+if hist_button:  # al hacer clic en el botón
     # escribir un mensaje
-    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    st.write(
+        'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
 
     # crear un histograma
     fig = px.histogram(car_data, x="odometer")
@@ -18,9 +20,9 @@ if hist_button: # al hacer clic en el botón
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
 
-scatter_button = st.button('Construir Scatter') # crear un botón
+scatter_button = st.button('Construir Scatter')  # crear un botón
 
-if scatter_button: # al hacer clic en el botón
+if scatter_button:  # al hacer clic en el botón
     # escribir un mensaje
     st.write('Creación de un gráfico de disperción para el conjunto de datos de anuncios de venta de coches')
 
